9ed53eebb3bc955a58e49ac555e4eac4ad0b40c6
Abdenour Anas Benkady <160033335+anasBenkady@users.noreply.github.com>
2024-05-08 09:13:08 +0100

    feat: [LKP-273] Move Public Email Providers list to sharedLibraries (#13)

--- CommonMessages/index.ts
+++ CommonMessages/index.ts
@@ -2,7 +2,7 @@ export { default as StatusCode } from "./utils/StatusCode";
 export { default as ApiError } from "./utils/ApiError";
 export { default as ErrorTypes } from "./utils/errorTypes";
 export { default as SuccessTypes } from "./utils/successTypes";
-
+export { default as publicEmailProviders } from "./utils/PublicEmailProviders";
 export { roles, roleList, hosts, HostRoleMapping, hostList} from "./utils/roles";
 export { talentStatus, nextStatus, statusList } from './utils/Status';
 export { errorHandlerMiddleware, routeNotFoundHandlerMiddleware , handleAsync} from "./middlewares/errorHandlers";
