c18338dc6e39363a6dd460d3db78a828a0acf030
Aymanebelg <aymane.belghacham@gmail.com>
2024-05-28 14:14:09 +0100

    test

--- CommonMessages/__tests__/middlewares/errorHAndlers.test.ts
+++ CommonMessages/__tests__/middlewares/errorHAndlers.test.ts
@@ -127,6 +127,6 @@ describe('errorHandlerMiddleware', () => {
     middleware(apiError, req, res, next)
 
     expect(res.status).toHaveBeenCalledWith(StatusCode.BAD_REQUEST)
-    expect(res.json).toHaveBeenCalledWith({ error: { name: ErrorTypes.BAD_REQUEST, details: 'Bad request'   } })
+    expect(res.json).toHaveBeenCalledWith({ error: { name: ErrorTypes.BAD_REQUEST, details: 'Bad request' } })
   })
 })
