eeb16f9959689be9005c5e4339e6117a29608eca
Aymanebelg <aymane.belghacham@gmail.com>
2024-05-29 10:12:43 +0100

    test

--- .github/scripts/run_tests.sh
+++ .github/scripts/run_tests.sh
@@ -2,15 +2,16 @@
 
 previous_commit=$1
 current_commit=$2
+command=$3
 
 echo "Checking changes between commits $previous_commit and $current_commit"
 for folder in $(ls -d */ | cut -f1 -d'/'); do
   if git diff --name-only $previous_commit $current_commit | grep -q "^$folder/"; then
     echo "$folder has changed"
-    echo "Running npm test in $folder"
+    echo "Running npm $command in $folder"
     cd $folder
     npm install
-    npm run test
+    npm run $command
     cd ..
   fi
 done
