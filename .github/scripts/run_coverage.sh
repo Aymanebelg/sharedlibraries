722e7542c291608b4296bdaba4f42657e9797a12
Aymanebelg <aymane.belghacham@gmail.com>
2024-05-29 10:14:25 +0100

    test

--- .github/scripts/run_coverage.sh
+++ .github/scripts/run_coverage.sh
@@ -7,10 +7,10 @@ echo "Checking changes between commits $previous_commit and $current_commit"
 for folder in $(ls -d */ | cut -f1 -d'/'); do
   if git diff --name-only $previous_commit $current_commit | grep -q "^$folder/"; then
     echo "$folder has changed"
-    echo "Running npm test in $folder"
+    echo "Running npm coverage in $folder"
     cd $folder
     npm install
-    npm run test
+    npm run coverage
     cd ..
   fi
 done
